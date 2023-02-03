a2a610dec1bc727db2ca60a309b88c2e003c1626
Ian Deladia <ian_deladia@medilink.ph>
2023-02-03 16:28:57 +0800

    Implement Services
    Add services/interfaces to reduce function to the controller (CharacterController)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 global using MYAPP.Models;
+using MYAPP.Services.CharacterService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
