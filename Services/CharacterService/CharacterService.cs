98ff6edbb6bda44b1e01fe2d29abfe7da4bbe7ba
Ian <kitsmype416@gmail.com>
2023-02-20 14:55:31 +0800

    Connect PSQL database and get implementation
    Added PSQL connection, Get implementation, dataContext and Initial migration

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MYAPP.Data;
 using MYAPP.Dtos.Character;
 
 namespace MYAPP.Services.CharacterService
@@ -15,9 +17,11 @@ namespace MYAPP.Services.CharacterService
         };
 
         private readonly IMapper _mapper;
-        public CharacterService(IMapper mapper)
+        private readonly DataContext _context;
+        public CharacterService(IMapper mapper, DataContext context)
         {
             _mapper = mapper;
+            _context = context;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
@@ -52,19 +56,25 @@ namespace MYAPP.Services.CharacterService
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             // throw new NotImplementedException();
-            return new ServiceResponse<List<GetCharacterDto>> { 
-                Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList()
-            };
+            // return new ServiceResponse<List<GetCharacterDto>> { 
+            //     Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList()
+            // };
+
+            var response = new ServiceResponse<List<GetCharacterDto>>();
+            var dbCharacter = await _context.Characters.ToListAsync();
+            response.Data = dbCharacter.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+
+            return response;
         }
 
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             // throw new NotImplementedException();
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
-            var character = characters.FirstOrDefault(c => c.Id == id);
+            var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
             serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
             return serviceResponse;
-        }
+        }   
 
         public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
         {
