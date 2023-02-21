402539d53bc12677f91abe17d304994e1a2f400e
Ian <kitsmype416@gmail.com>
2023-02-21 11:59:19 +0800

    User Registration Implementation
    User Registration implementation and checking if the user already exists.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MYAPP.Data;
+using MYAPP.Dtos.User;
+
+namespace MYAPP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepo.Register(
+                new User { Username = request.Username}, request.Password
+            );
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
